36f88ca693bb875ad05a64830e91c446ae9c9275
Andresini5858 <lem21634@uvg.edu.gt>
2023-02-09 01:48:44 -0600

    Creación

---
+++ ed2_4_master.X/main_master.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main_master.c
+ * Author: Andres
+ *
+ * Created on February 8, 2023, 12:27 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ ed2_4_slave1.X/main_slave1.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main_slave1.c
+ * Author: Andres
+ *
+ * Created on February 9, 2023, 1:00 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
